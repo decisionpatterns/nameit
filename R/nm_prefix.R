4fea56c811f61393a97c485dbbf37d06d4e65f26
Christopher Brown <cbrown@decisionpatterns.com>
2019-02-15 17:31:14 -0800

    v0.4.1: Fix docs

--- R/nm_prefix.R
+++ R/nm_prefix.R
@@ -1,56 +1,56 @@
-#' Add a prefix or suffix to the names of an object 
-#' 
-#' Adds a prefix or suffix to the names attributes. 
-#' 
+#' Add a prefix or suffix to the names of an object
+#'
+#' Adds a prefix or suffix to the names attributes.
+#'
 #' @param x object with a names attribute
-#' @param prefix character; prefix collapsed to string by \code{sep} and 
-#' added to the front of each name 
-#' @param suffix character; collapsed to string by \code{sep} and 
+#' @param prefix character; prefix collapsed to string by `sep` and
+#' added to the front of each name
+#' @param suffix character; collapsed to string by `sep` and
 #' added to the back of each name.
-#' @param sep string used to collapse and seperate \code{prefix} or 
-#' \code{suffix} from names. The default is not to use a separator.
-#' @param exclude_key logical; if \code{TRUE} do not mangle the object's keys. 
-#' (DEFAULT: \code{TRUE}) 
+#' @param sep string used to collapse and seperate `prefix` or
+#' `suffix` from names. The default is not to use a separator.
+#' @param exclude_key logical; if `TRUE` do not mangle the object's keys.
+#' (DEFAULT: `TRUE`)
 #' @param ... additional arguments
 #'
-#' @details 
-#' 
-#' \code{nm_prefix} and \code{nm_suffix} add a prefix or suffix to the names of
-#' an object. 
-#' 
+#' @details
+#'
+#' `nm_prefix` and `nm_suffix` add a prefix or suffix to the names of
+#' an object.
+#'
 #' The method for data.table objects are modified by reference.
-#' 
+#'
 #' @return
-#'   An object with the names altered by a prefix or suffix. For data.tables, 
+#'   An object with the names altered by a prefix or suffix. For data.tables,
 #'   these are returned invisibly.
-#'   
-#' @seealso 
-#'   \code{\link[stringr.tools]{str_prefix}} which is used by these functions
-#'   
+#'
+#' @seealso
+#'   [stringr.tools::str_prefix()] which is used by these functions
+#'
 #' @examples
-#' 
+#'
 #' # nm_prefix
-#'   
+#'
 #'   data(iris)
-#'   iris <- nm_prefix( iris, "var_" ) 
+#'   iris <- nm_prefix( iris, "var_" )
 #'   names(iris)
-#'   
-#'   data(iris)
-#'   setDT(iris)
-#'   nm_prefix( iris, "var_" ) 
-#'   names(iris)
-#' 
-#' # nm_suffix      
-#'   data(iris)
-#'   iris <- nm_suffix( iris, "_var" ) 
-#'   names(iris)
-#'   
+#'
 #'   data(iris)
 #'   setDT(iris)
-#'   nm_suffix( iris, "_var" ) 
+#'   nm_prefix( iris, "var_" )
 #'   names(iris)
-#'   
-#' @rdname nm_prefix  
+#'
+#' # nm_suffix
+#'   data(iris)
+#'   iris <- nm_suffix( iris, "_var" )
+#'   names(iris)
+#'
+#'   data(iris)
+#'   setDT(iris)
+#'   nm_suffix( iris, "_var" )
+#'   names(iris)
+#'
+#' @rdname nm_prefix
 #' @import stringr.tools
 #' @export
 
@@ -58,42 +58,43 @@
 # ---------------------------------------------------------------
 
 nm_prefix <- function(x, prefix, sep="", ... ) {
-  if( is.null( names(x) ) ) 
+  if( is.null( names(x) ) )
     stop( "There is no names attribute for ", deparse(substitute(x)))
   UseMethod( "nm_prefix" )
 }
 
 
 
-#' @rdname nm_prefix 
+#' @rdname nm_prefix
 #' @export
 
 nm_prefix.default <- function(x, prefix, sep="", ...) {
-  
+
   ext <- paste( prefix, collapse=sep )  # Determine extension to be added
-  
+
   names(x) <- names(x) %>% str_prefix( ext, sep=sep )
   return(x)
-} 
+}
 
 
 #' @rdname nm_prefix
 #' @export
 
 nm_prefix.data.frame <- function(x, prefix, sep="", ...) {
-  
+
   ext <- paste( prefix, collapse=sep )  # Determine extension to be added
-  
+
   names(x) <- names(x) %>% str_prefix( ext, sep=sep )
   return(x)
-} 
+}
 
-#' @seealso 
-#'   \code{\link[data.table]{setnames}}
-# @note 
-#    It may be advantagous to return the data.table, \code{x} here rather 
+#' @seealso
+#'   (setnames)[data.table::setattr()]
+#'
+# @note
+#    It may be advantagous to return the data.table, `x` here rather
 #    than invisible(NULL)
-# 
+#
 #' @rdname nm_prefix
 #' @import data.table
 #' @export
@@ -101,39 +102,39 @@ nm_prefix.data.frame <- function(x, prefix, sep="", ...) {
 nm_prefix.data.table <- function(x, prefix, sep="", exclude_key = TRUE ) {
 
   nms <- names(x)
-  if( exclude_key && ! is.null(key(x)) ) { 
-    nms <- nms %>% setdiff( key(x) )  
+  if( exclude_key && ! is.null(key(x)) ) {
+    nms <- nms %>% setdiff( key(x) )
   }
-  
+
   ext <- paste( prefix, collapse=sep )  # Determine extension to be added
-  
+
   setnames(
-    x, 
+    x,
     nms,
     nms %>% str_prefix(ext, sep)
   )
 
   return(invisible(x))
-  
-}  
+
+}
 
 
 # SUFFIX #
 
 nm_suffix <- function(x, suffix, sep="", ... ) {
-  if( is.null( names(x) ) ) 
+  if( is.null( names(x) ) )
     stop( "There is no names attribute for ", deparse(substitute(x)))
   UseMethod( "nm_suffix" )
 }
 
-#' @rdname nm_prefix 
+#' @rdname nm_prefix
 #' @export
 
 nm_suffix.default <- function(x, suffix, sep="", ...) {
   ext <- paste( suffix, collapse=sep )  # Determine extension to be added
   names(x) <- names(x) %>% str_suffix( ext, sep=sep )
   return(x)
-} 
+}
 
 
 #' @rdname nm_prefix
@@ -143,31 +144,32 @@ nm_suffix.data.frame <- function(x, suffix, sep="", ...) {
   ext <- paste( suffix, collapse=sep )  # Determine extension to be added
   names(x) <- names(x) %>% str_suffix( ext, sep=sep )
   return(x)
-} 
+}
 
-#' @seealso 
-#'   \code{\link[data.table]{setnames}}
-# @note 
-#    It may be advantagous to return the data.table, \code{x} here rather 
+#' @seealso
+#'   (data.table::setnames)[data.table::setattr()]
+#'
+# @note
+#    It may be advantagous to return the data.table, `x` here rather
 #    than invisible(NULL)
-# 
+#
 #' @rdname nm_prefix
 #' @export
 
 nm_suffix.data.table <- function(x, suffix, sep="", ..., exclude_key ) {
-  
+
   nms <- names(x)
-  if( exclude_key && ! is.null(key(x)) ) { 
-    nms <- nms %>% setdiff( key(x) )  
+  if( exclude_key && ! is.null(key(x)) ) {
+    nms <- nms %>% setdiff( key(x) )
   }
-  
+
   ext <- paste( suffix, collapse=sep )  # Determine extension to be added
-  
+
   setnames(
-    x, 
+    x,
     nms,
     nms %>% str_suffix(ext, sep)
   )
 
   return(invisible(x))
-}  
+}
